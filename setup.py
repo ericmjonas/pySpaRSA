2aa7acb99ff14c3f2feaf97627d9ff59637e20d8
Eric jonas <jonas@ericjonas.com>
2014-10-13 14:05:29 -0700

    initial code org

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='pySpaRSA',
+      version='1.0',
+      description='Python implementation of SpaRSA',
+      author='Eric Jonas',
+      author_email='jonas@ericjonas.com',
+      url='http://github.com/ericmjonas/pysparsa/',
+      packages=['pysparsa'],
+     )
